f101c73825abeb02fbc9451baffbf0a42e766607
Oualid Hassani <ohassani@e2r6p3.1337.ma>
2024-11-07 14:28:24 +0100

    have some issues in withdraw

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -40,6 +40,7 @@ int	Account::getNbWithdrawals(void)
 
 void Account::displayAccountsInfos(void) 
 {
+    _displayTimestamp();
     std::cout << "accounts:" << getNbAccounts() << ";total:" << getTotalAmount()
               << ";deposits:" << getNbDeposits() << ";withdrawals:" << getNbWithdrawals() << std::endl;
 }
@@ -51,4 +52,35 @@ void	Account::_displayTimestamp(void)
     time_display = time(NULL);
 
     std::cout << "[" << time_display << "] "; 
+}
+
+void	Account::displayStatus( void ) const
+{
+    _displayTimestamp();
+    std::cout << "index:" << _accountIndex << ";amount:" << _amount << ";deposits:" << _nbDeposits << ";withdrawals:" << _nbWithdrawals << std::endl;
+}
+
+
+void	Account::makeDeposit(int deposit)
+{
+    _nbDeposits++;
+    _totalNbDeposits++;
+    _totalAmount += deposit;
+    _amount += deposit;
+    _displayTimestamp();
+    std::cout << "index:" << _accountIndex << ";p_amount:" << _amount - deposit << ";deposit:" << deposit << ";amout:" << _amount << ";nb_deposits:" << _nbDeposits << std::endl;  
+     std::cout << std::endl ;
+     std::cout << std::endl  << "--------------->";
+
+}
+
+bool	Account::makeWithdrawal(int withdrawal)
+{
+    _displayTimestamp();
+    
+    std::cout << "index:" << _accountIndex << ";amout:" << _amount <<  ";deposits:" << _nbDeposits << ";withdrawls:" <<  withdrawal << std::endl;
+
+     std::cout << std::endl << "<---------------";
+    return(false);
+
 }
