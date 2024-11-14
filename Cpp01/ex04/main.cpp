d6cbcdaeab83cc2b5b6478955040469963d46d85
Oualid Hassani <ohassani@e2r7p4.1337.ma>
2024-11-14 22:08:00 +0100

    starting ex04

--- Cpp01/ex04/main.cpp
+++ Cpp01/ex04/main.cpp
@@ -4,8 +4,27 @@ int main(int ac, char **av)
 {
     if(ac == 4)
     {
+        std::string file;
+        std::string s1;
+        std::string s2;
         
-    }
+        file = av[1];
+        s1 = av[2];
+        s2 = av[3];
+
+        std::fstream Filename;
+        Filename.open(file, std::ios::out);        
+        if(Filename) 
+        {
+            std::string content;
+            std::getline(Filename, content);
+
+            
+            
+        }
+        else
+            std::cout << "fail to open the file";
+    }   
     else
-        
+        std::cout << "need to enter arguments like this: <file name> s1 s2";
 }
--- Cpp01/ex04/main.hpp
+++ Cpp01/ex04/main.hpp
@@ -3,4 +3,5 @@
 
 #include <iostream>
 #include <fstream>
+#include <string>
 #endif
