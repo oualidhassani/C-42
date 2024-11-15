7c5b614ca02f05f7fddf49508142fb4efc7fc8d2
Oualid Hassani <ohassani@e2r2p8.1337.ma>
2024-11-15 12:42:26 +0100

    is done with ex04 mybe more check for parsing

--- Cpp01/ex04/main.cpp
+++ Cpp01/ex04/main.cpp
@@ -7,24 +7,57 @@ int main(int ac, char **av)
         std::string file;
         std::string s1;
         std::string s2;
-        
         file = av[1];
         s1 = av[2];
         s2 = av[3];
-
-        std::fstream Filename;
-        Filename.open(file, std::ios::out);        
-        if(Filename) 
+        
+        if(s1.empty() || s2.empty())
         {
-            std::string content;
-            std::getline(Filename, content);
-
-            
-            
+            std::cout << "there is an empty string " << std::endl;
+            return 1;
         }
-        else
-            std::cout << "fail to open the file";
-    }   
+        
+        std::ifstream inFile(file); 
+        
+        if(!inFile)
+        {
+            std::cout << "Failed to open the file" << std::endl;
+            return 1;
+        }
+        
+        std::string content;
+        std::string line;
+        
+        while(getline(inFile, line))
+        {
+            content += line + "\n";
+        }
+        inFile.close();
+        
+        size_t pos = 0;
+        while((pos = content.find(s1, pos)) != std::string::npos)
+        {
+            content.erase(pos, s1.length());
+            content.insert(pos, s2);
+            pos += s2.length();
+        }
+        
+        if (!content.empty() && content.back() == '\n')
+            content.pop_back();
+            
+        std::string outfile = file + ".replace";
+        std::ofstream Outfile(outfile);
+        if(!Outfile)
+        {
+            std::cout << "Failed to open the file" << std::endl;
+            return 1;
+        }
+        
+        Outfile << content;
+        Outfile.close();
+    }
     else
-        std::cout << "need to enter arguments like this: <file name> s1 s2";
+        std::cout << "need to enter arguments like this: <file name> s1 s2" << std::endl;
+    
+    return 0;
 }
